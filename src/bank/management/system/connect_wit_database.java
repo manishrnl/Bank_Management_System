910f2293cdd65916e5eafd50ad897adbd8a0fe73
Manish Kumar <79047406+manishrnl@users.noreply.github.com>
2025-01-19 14:33:09 +0530

    Update connect_wit_database.java

--- src/bank/management/system/connect_wit_database.java
+++ src/bank/management/system/connect_wit_database.java
@@ -1,3 +1,4 @@
+/*  Write this code to run on Local MYSQL System
 package bank.management.system;
 
 import javax.swing.*;
@@ -20,5 +21,36 @@ public class connect_wit_database {
     }
 
     public static class MiniStatement {
+    }
+}
+*/
+
+//  Use this to run on Online server using  MYSQL. All the table is same as in local Machine
+package bank.management.system;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class connect_wit_database {
+    Connection connections;
+    Statement statement;
+
+    public connect_wit_database() {
+        try {
+            //Connecting with Mysql database
+            String url = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12758451";
+            String user = "sql12758451";
+            String password = "YHcSXW42B5";
+            connections = DriverManager.getConnection(url, user, password);
+            statement = connections.createStatement();
+        } catch (Exception E) {
+            JOptionPane.showMessageDialog(null, "Something Went Wrong");
+        }
+    }
+
+    public static void main(String[] args) {
+        new bank.management.system.connect_wit_database();
     }
 }
